f3d58cc647f1d3644881d732a92d70573e81e25b
BeProact <jasonalow8@gmail.com>
2025-12-28 07:15:21 -0500

    Initial Commit

---
+++ home.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, ...}:
+
+{
+    home.username = "snappy";
+    home.homeDirectory = "/home/snappy";
+    programs.git.enable = true;
+    home.stateVersion = "25.11";
+    programs.bash = {
+        enable = true;
+        shellAliases = {
+            nrs = "sudo nixos-rebuild switch";
+            btw = "echo banana";
+            nrsf = "sudo nixos-rebuild switch --flake /home/snappy/.dotfiles/nixos/ --impure";
+            nrsi = "sudo nixos-rebuild switch --flake . --impure";
+        };
+        initExtra =
+            ''
+                function y() {
+                    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+                    yazi "$@" --cwd-file="$tmp"
+                    IFS= read -r -d ''' cwd < "$tmp"
+                    [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+                    rm -f -- "$tmp"
+                }
+            '';
+    };
+
+    programs.git = {
+        settings = {
+            user = {
+                name  = "BeProact";
+                email = "jasonalow8@gmail.com";
+            };
+            init.defaultBranch = "main";
+        };
+    };
+
+    programs.ghostty = {
+        enable = true;
+        settings = {
+            font-family = "JetBrainsMono Nerd Font";
+        };
+    };
+}
